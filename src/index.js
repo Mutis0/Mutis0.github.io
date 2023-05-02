ef506c0c0e272b311fe3d5038b34f6f0a88804b0
Mutis0 <127284295+Mutis0@users.noreply.github.com>
2023-05-02 19:06:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+initializePage();
+
+const poster = documents.getElementById('poster');
 const header = document.querySelector('.header');
 const menuBtn = header.querySelector('.menu-btn');
 const menuIcons = header.querySelector('.menu-icons');
+
+function fetchShopProducts() {
+  fetch ("http://localhost:3000/shop")
+  .then(response => response.json()) 
+  .then(products => {
+    renderPosterDetails(products);
+  })
+}
 
 menuBtn.addEventListener('click', () => {
   menuIcons.classList.toggle('show');
 });
 
+function initializePage(){
+  fetchShopProducts();
+}
